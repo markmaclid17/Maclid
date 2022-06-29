04d30bc1a58e05b3709ef498605f1dc55c6e1c5d
Maclids <markmaclid@gmail.com>
2022-06-29 14:49:41 +0800

    maclid

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace agay.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
